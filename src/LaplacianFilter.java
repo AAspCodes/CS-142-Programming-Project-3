6dd217963543c3e0c3fab087c86f9c251d9fd42e
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-01 13:41:57 -0800

    Create LaplacianFilter.java

---
+++ src/LaplacianFilter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Anthony Asp, Emily
+ * 
+ * Filter that converts image using Laplacian filter technique.
+ *
+ */
+public class LaplacianFilter implements Filter {
+	int[][] weights = {{-1,-1,-1},
+			   		   {-1,8,-1},
+			   		   {-1,-1,-1}};
+	int scaleFactor = 1;
+
+	public void filter(PixelImage pi) {
+		Pixel[][] data = pi.getData();
+		pi.computeFilter(data, weights, scaleFactor);
+		pi.setData(data);
+	}
+
+}
