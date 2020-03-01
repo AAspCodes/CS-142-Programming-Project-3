0ee933eab82930cf2c6e9c474f28131f048b1f05
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-01 12:57:17 -0800

    Create GaussianFilter.java

---
+++ src/GaussianFilter.java
@@ -0,0 +1,15 @@
+
+public class GaussianFilter implements Filter {
+	int[][] weights = {{1,2,1},
+					   {2,4,2},
+					   {1,2,1}};
+	int scaleFactor = 16;
+	
+	
+	public void filter(PixelImage pi) {
+		Pixel[][] data = pi.getData();
+		pi.computeFilter(data, weights, scaleFactor);
+		pi.setData(data);
+	}
+
+}
