2cc8d283063a4970665fb57f3f2919256bdb5e29
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-02 09:28:55 -0800

    Update PixelImage.java

--- src/PixelImage.java
+++ src/PixelImage.java
@@ -95,7 +95,7 @@ public class PixelImage {
 	// add a method to compute a new image given weighted averages
 	public Pixel[][] computeFilter(Pixel[][] data, int[][] weights, int scaleFactor) {
 		Pixel[][] newData = new Pixel[data.length][data[0].length];
-
+		// TODO fix edge creep bug!!
 		for (int i = 0; i < newData.length; i++) {
 			for (int j = 0; j < newData[0].length; j++) {
 				newData[i][j] = new Pixel(0, 0, 0);
