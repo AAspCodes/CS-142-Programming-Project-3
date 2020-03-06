3be0d4d9b90cc993956a52432a526eace70e6e18
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-06 14:21:18 -0800

    add Gauss5x5Filter

---
+++ src/Gauss5by5.java
@@ -0,0 +1,18 @@
+
+public class Gauss5by5 implements Filter {
+	int[][] weights = {{1,2,3,2,1},
+			   		   {2,4,6,4,2},
+			   		   {3,6,9,6,3},
+			   		   {2,4,6,4,2},
+			   		   {1,2,3,2,1}};
+	
+	int scaleFactor = 81;
+			
+	
+	
+	public void filter(PixelImage pi) {
+		pi.computeSummingFilter(weights, scaleFactor);
+
+	}
+
+}
--- src/SnapShopConfiguration.java
+++ src/SnapShopConfiguration.java
@@ -24,6 +24,7 @@ public class SnapShopConfiguration {
 		theShop.addFilter(new LaplacianFilter(), "Laplacian Filter");
 		theShop.addFilter(new UnsharpMaskingFilter(), "Unsharp Masking Filter");
 		theShop.addFilter(new EdgyFilter(), "Edgy Filter");
+		theShop.addFilter(new Gauss5by5(), "Gauss 5x5 Filter");
 		// add your other filters below
 	}
 }
