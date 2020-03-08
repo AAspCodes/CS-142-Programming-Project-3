7bdfe2fd1d5936e28917d403e7bba67518f01b2e
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-08 12:26:14 -0700

    renaming

--- src/Gaussian5x5Filter.java
+++ src/Gaussian5x5Filter.java
@@ -3,7 +3,8 @@
  *
  *Filter the image using Gaussian Blur. 5x5
  */
-public class Gauss5x5Filter implements Filter {
+
+public class Gaussian5x5Filter implements Filter {
 	int[][] weights = {{1,2,3,2,1},
 			   		   {2,4,6,4,2},
 			   		   {3,6,9,6,3},
