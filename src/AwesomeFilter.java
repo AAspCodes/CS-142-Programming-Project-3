f146a27f2a543a91b25935ccdff5764e276b6634
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-08 14:52:37 -0700

    Update AwesomeFilter.java

--- src/AwesomeFilter.java
+++ src/AwesomeFilter.java
@@ -3,6 +3,12 @@ public class AwesomeFilter implements Filter {
 
 	@Override
 	public void filter(PixelImage theImage) {
+		Mean7x7Filter meanf = new Mean7x7Filter();
+		for (int i = 0; i<4;i++) {
+			meanf.filter(theImage);
+		}
+		
+		
 		Median7x7Filter medf = new Median7x7Filter();
 		for (int i = 0; i<4; i++) {
 			medf.filter(theImage);
