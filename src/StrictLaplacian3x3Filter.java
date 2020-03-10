3e611c856b3369b7a7d2ea4666e3c481431c0b4d
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-10 11:12:18 -0700

    Update StrictLaplacian3x3Filter.java
    add header comment

--- src/StrictLaplacian3x3Filter.java
+++ src/StrictLaplacian3x3Filter.java
@@ -1,3 +1,11 @@
+/**
+ * 
+ * @author Anthony Asp, Emily Plott
+ * 
+ * This Filter is a more strict version of laplacian that only takes
+ * the values directly above and beside the central pixel.
+ * It ignore the pixels diagonal from the center.
+ */
 
 public class StrictLaplacian3x3Filter implements Filter {
 	int[][] weights = {{0,-1,0},
