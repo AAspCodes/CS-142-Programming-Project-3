bf63fc57eb26e78acd9b8e2cc52e638183b5b73f
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-10 17:41:31 -0700

    Update SpeedTestFilter.java

--- src/SpeedTestFilter.java
+++ src/SpeedTestFilter.java
@@ -20,13 +20,11 @@
  */
 
 public class SpeedTestFilter implements Filter {
-
-	
 	public void filter(PixelImage theImage) {
 		long totalTime = 0;
 		
-		
 		Gaussian5x5Filter gf = new Gaussian5x5Filter();
+		
 		for (int i = 0; i < 100; i++) {
 			long start = System.nanoTime();
 			gf.filter(theImage);
