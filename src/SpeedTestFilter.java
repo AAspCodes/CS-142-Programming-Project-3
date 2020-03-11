fba4e6c4e102f41b885c0df28283f1dab88aa60a
Anthony Asp <46567012+AAspCodes@users.noreply.github.com>
2020-03-10 17:41:08 -0700

    add header comment

--- src/SpeedTestFilter.java
+++ src/SpeedTestFilter.java
@@ -1,3 +1,23 @@
+/**
+ * 
+ * @author Anthony Asp, Emily Plott
+ * 
+ * this is a testing filter to look for speed improvements that can
+ *  be made without adding more overhead in each filter.
+ *  
+ *  Surpisingly, We were unble to make much progress, 13.5 seconds
+ *  on anthony's computer was the average for a 622x1000 pixel image.
+ *  
+ *  We tried replaceinng the getColor method with 3 seperate compute pixel method(one for each color).
+ *  That only made very marginal improvements.
+ *  
+ *  We tried completely replaceing the pixel class with an int array of length 3.
+ *  That actually slowed things down by 3 seconds.
+ *  
+ *  Perhaps using hash tables could create some improvements.
+ *  switching from int to unsigned byte would probably improve speed. 
+ *
+ */
 
 public class SpeedTestFilter implements Filter {
 
